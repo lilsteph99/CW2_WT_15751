000cfed199f2e3d63881dea12bce5d548adc4a07
Sunnat <sunnatturgunovno1@gmail.com>
2024-03-11 20:27:32 +0500

    added some logic

--- controllers/web/home/index.js
+++ controllers/web/home/index.js
@@ -1,23 +1,16 @@
-const home_controller = {
+const ticket_service = require('../../../services/ticket')
 
+const home_controller = {
     index: async (req, res) =>{
-    
-    res.render('home');
-    
+        res.render('home');
     },
-    
     add: async (req, res) =>{
-    
-    res.render('home/add_update');
-    
+        res.render('home/add_update', { mode: 'Add' });
     },
-    
     update: async (req, res) =>{
-    
-    res.render('home/add_update');
-    
+        const eventData = await ticket_service.getById(req.params.id);
+        res.render('home/add_update', { mode: 'Update', eventData: eventData });
     }
-    
-    };
-    
-    module.exports = home_controller;
+};
+  
+module.exports = home_controller;
